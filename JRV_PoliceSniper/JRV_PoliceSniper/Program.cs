f0a62f0ad15780062c7a69fe9d50615b41e2591e
jrvaughan917 <j_r_vaughan@yahoo.com>
2017-01-20 15:35:24 -0600

    Add files via upload

--- JRV_PoliceSniper/JRV_PoliceSniper/Program.cs
+++ JRV_PoliceSniper/JRV_PoliceSniper/Program.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/*
+ * JRV_PoliceSniper by James R. Vaughan 
+ * 
+ * A simple game created in Visual C#
+ * 
+ * Bad guys have overrun the building!
+ * Neutralize the bad guys by clicking on them.
+ * If you click on a good guy, the game is over.
+ * As time progresses, bad guys and good guys appear and disappear more quickly, making the game more challenging.
+ *
+ */
+
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
